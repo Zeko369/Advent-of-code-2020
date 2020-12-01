7f5fc70589c877f62b16fd630c3cd4c9383d0ce4
Fran Zekan <zekan.fran369@gmail.com>
2020-12-02 00:31:14 +0100

    Update jsdoc

--- common/input.js
+++ common/input.js
@@ -1,10 +1,15 @@
 const { readFile } = require("fs").promises;
 
-module.exports = async (filename, config = {}) => {
+/**
+ * getLines function used to read from input file
+ * @param {string} filename
+ * @param {{mapperFunction: (data: string, index: number) => any}} [config] Config object
+ */
+module.exports = async (filename, config) => {
   let lines = [];
   try {
     lines = (await readFile(filename, "utf-8")).split("\n");
-    lines = lines.map((line) => line.trim());
+    lines = lines.map((line) => line.trim()).filter(Boolean);
   } catch (err) {
     throw new Error("no_file");
   }
