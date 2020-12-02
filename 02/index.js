027fffea7a6c8a7499b62e904b91b01d865abb60
Fran Zekan <zekan.fran369@gmail.com>
2020-12-02 06:12:50 +0100

    Add day 2

--- 02/index.js
+++ 02/index.js
@@ -3,11 +3,51 @@ const getLines = require("../common/input");
 
 (async () => {
   /**
-   * @type {any[]}
+   * @type {string[][]}
    */
   const input = await getLines(join(__dirname, "input.txt"), {
-    mapperFunction: (row) => parseInt(row),
+    mapperFunction: (row) => row.split(" "),
   });
 
   // ENTER CODE HERE
+
+  let globalCount = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const [count, letter, password] = input[i];
+    const a = letter[0];
+
+    let tmp = 0;
+    for (let j = 0; j < password.length; j++) {
+      if (a === password[j]) {
+        tmp++;
+      }
+    }
+
+    const [from, to] = count.split("-").map((a) => parseInt(a));
+
+    if (from <= tmp && to >= tmp) {
+      globalCount++;
+    }
+  }
+
+  console.log(globalCount);
+
+  globalCount = 0;
+  for (let i = 0; i < input.length; i++) {
+    const [count, letter, password] = input[i];
+    const a = letter[0];
+
+    const [from, to] = count.split("-").map((a) => parseInt(a) - 1);
+
+    let tmp = 0;
+    if (a === password[from]) tmp++;
+    if (a === password[to]) tmp++;
+
+    if (tmp === 1) {
+      globalCount++;
+    }
+  }
+
+  console.log(globalCount);
 })();
